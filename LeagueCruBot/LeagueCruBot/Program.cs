a67e7fc0bf9921488a8901afa843e3d46cbeed22
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2018-05-03 00:45:48 -0400

    LC: Initial Commit

---
+++ LeagueCruBot/LeagueCruBot/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+//importing our project dependencies for Discord
+using Discord;
+using Discord.WebSocket;
+
+//we use this using statement to see everything inside the config folder
+using LeagueCruBot.Util;
+
+namespace LeagueCruBot
+{
+    class Program
+    {
+        //Declare variable of types DiscordSocketClient and CommandHandler
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+        private string _token;
+        private string _botFilesPath;
+
+        //Turn this program into an asynchronous program
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            //Set the Console Title for the application 
+            Console.Title = "League Cru Bot 1.0";
+
+            //Call the static method "StartupOpertations" in the static class "Support"
+            Support.StartupOperations();
+            //This will set our token equal to token found in the App.config file
+            _token = Support.config.Get("botToken");
+            //This will set our default bot files directory to what is found in the App.config file
+            _botFilesPath = Support.config.Get("botFilesPath");
+
+            //Instantiate a new Instance of DiscordSocketClient object
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                //This line tells you how thorough the logging will be, verbose is generally good for most applications. 
+                //For more detailed information, user LogSeverity.Debug
+                LogLevel = LogSeverity.Verbose
+            });
+
+            //Logs in using the token that was given into the JSON file. 
+            await _client.LoginAsync(TokenType.Bot, _token);
+            //Start the bot
+            await _client.StartAsync();
+            //Sets the bot's "Playing" status to "!help for commands" to let people in the server know how to use the help command
+            await _client.SetGameAsync("!help for commands");
+
+            //Instantiate a new instance of CommandHandler class. 
+            _handler = new CommandHandler();
+
+            //Passes the _client object into the _handler object
+            await _handler.InstallAsync(_client);
+
+            //Write log messages to the console.
+            _client.Log += (logMessages)
+                       => Console.Out.WriteLineAsync(logMessages.ToString());
+
+            //Make sure the console does not exit.
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ LeagueCruBot/LeagueCruBot/Util/CommandHandler.cs
@@ -0,0 +1,94 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace LeagueCruBot.Util
+{
+    public class CommandHandler
+    {
+        //Declare a _client variable type DiscordSocketClient
+        private DiscordSocketClient _client;
+
+        //Declare _service cariable type CommandServnce
+        private CommandService _service;
+
+
+        //Sets up the CommandService
+        public async Task InstallAsync(DiscordSocketClient client)
+        {
+            //Set up _client variable to the client object we passed in from the main program
+            this._client = client;
+
+            //Instantiates a new CommandService object
+            this._service = new CommandService();
+
+            //Not too sure how the command service works yet
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            //on message recieved event, execute this async task
+            _client.MessageReceived += HandleCommandAsync;
+
+            //on user join server event, execute this async task
+            _client.UserJoined += AnnounceUserJoined;
+        }
+
+
+        //Method to handle what to do when we recieve a message, takes the discord messages as a parameter
+        public async Task HandleCommandAsync(SocketMessage UserMsg)
+        {
+            //Makes sure this message isn't coming from a bot or anything like that, explicitly cast the SocketMessage into a SocketUserMessage
+            SocketUserMessage msg = UserMsg as SocketUserMessage;
+
+            //If the message is empty, don't even bother
+            if (msg == null)
+                return;
+
+            //Instantiate new SocketCommandContext object with parameters taking in the _client and the message we defined earlier
+            SocketCommandContext context = new SocketCommandContext(_client, msg);
+
+            //define variable argPos of type integer with a value of 0
+            int argPos = 0;
+
+            //Check the first character of a message. If it has the '!' in front of it, we will take it as a command
+            if (msg.HasCharPrefix('!', ref argPos))
+            {
+
+                //Attempt to execute a command with the message, store the results of that command in a variable called result of type IResult
+                IResult result = await _service.ExecuteAsync(context, argPos);
+
+                //If the result is not successful and the result isn't the fact that it's an unknown command, send the error reason back to the channel where
+                //The command was read
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+
+
+        public async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            //The bot is only going to be used in League Cru's Discord server but might as well make it so that the bot can go to any server and use
+            //the name of whatever server it goes to, anyways.. this gets the name of the server that the user just joined
+            string guildName = user.Guild.Name;
+
+            //Declare a variable called channel of type SocketTextChannel
+            SocketTextChannel channel;
+
+            //Check to see if the Server's guild ID matches League Cru Discord Server's guild ID, if it matches, then the user is in the League
+            //Cru Discord Server
+            if (user.Guild.Id == 114439001703710725)
+                //Set the channel variable to a channel with specified channel id
+                channel = _client.GetChannel(114439001703710725) as SocketTextChannel;
+            else
+                //If the server the user joined isn't the League Cru server, use their default channel (Interestingly enough,
+                //the default channel of a discord server is a channel with the same ID as the server itself. Assumably this is the first text channel
+                //To get created upon server creation)
+                channel = user.Guild.DefaultChannel;
+            //Welcome the user to the server
+            await channel.SendMessageAsync($"Welcome to {guildName}, {user.Mention}!");
+        }
+    }
+}
